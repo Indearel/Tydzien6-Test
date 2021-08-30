a06514993973765ad01d37533e3cfd9535836d94
Indygos <cyberindygos@gmail.com>
2021-08-30 19:05:54 +0200

    test

--- src/components/TimeboxList.js
+++ src/components/TimeboxList.js
@@ -6,9 +6,9 @@ import TimeboxCreator from "./TimeboxCreator";
 class TimeboxList extends React.Component {
     state = {
         timeboxes: [
-            { id: "a", title: "Uczę się o console", totalTimeInMinutes: 25 },
-            { id: "b", title: "Uczę się debugować", totalTimeInMinutes: 15 },
-            { id: "c", title: "Uczę się TDD", totalTimeInMinutes: 5 },
+            { id: "a", title: "Uczę się githuba", totalTimeInMinutes: 25 },
+            { id: "b", title: "Uczę się bibliotek", totalTimeInMinutes: 15 },
+            { id: "c", title: "Uczę się komytuf", totalTimeInMinutes: 5 },
         ]
     }
     
